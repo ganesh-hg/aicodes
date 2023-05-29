94af01a09bcef51dcb88186c3477efcd019e5901
Ganesh Hale <78199996+ganesh-hg@users.noreply.github.com>
2023-05-29 09:47:25 +0530

    dfs.py

--- dfs.py
+++ dfs.py
@@ -20,3 +20,23 @@ print("DFS:")
 dfs(visited, graph, '5')
 
 # Output:
+
+'''  The code defines a graph using a dictionary called graph, where the keys represent nodes in the graph, and the values are lists of adjacent nodes.
+
+An empty list visited is initialized to keep track of the visited nodes during the DFS traversal.
+
+The dfs function is defined to perform the DFS traversal. It takes three parameters: visited (the list of visited nodes), graph (the adjacency list representation of the graph), and node (the current node being visited).
+
+Inside the dfs function, there is a base condition that checks if the current node is not in the visited list. If it's not visited, it means we have encountered a new node.
+
+The new node is then printed (print(node)) to show the order in which nodes are visited during the DFS traversal. This line can be modified or removed depending on how you want to use the DFS traversal.
+
+The current node is then added to the visited list to mark it as visited.
+
+The function then recursively calls itself for each neighbor of the current node (obtained from the graph dictionary). This recursive call is made only for unvisited neighbors.
+
+The DFS traversal starts by calling the dfs function with the initial node to begin the traversal. In this case, it starts with node '5'.
+
+The output will be the order in which nodes are visited during the DFS traversal.
+
+In summary, the code performs a Depth-First Search (DFS) traversal on a graph represented as an adjacency list. It visits each node in the graph and explores its neighbors recursively until all reachable nodes are visited. The output shows the order in which nodes are visited during the DFS traversal, starting from the given initial node. '''
