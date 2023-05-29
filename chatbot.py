f27e055ad72320d0c2d27f063acf9cd289f8c32b
Ganesh Hale <78199996+ganesh-hg@users.noreply.github.com>
2023-05-29 09:49:29 +0530

    chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -48,3 +48,25 @@ while True:
     user_input = input("User: ")
     bot_response = get_response(user_input)
     print("Bot:", bot_response)
+    
+    
+    ''' The code defines two dictionaries: greetings and queries. The greetings dictionary maps possible user greetings (keys) to corresponding bot responses (values). Similarly, the queries dictionary maps possible user queries (keys) to bot responses (values). These dictionaries provide the chatbot with predefined responses for different types of user inputs.
+
+The get_response function takes a user input as a parameter and returns a bot response based on the input. It performs the following steps:
+
+Converts the user input to lowercase using user_input.lower() to make it case-insensitive.
+Initializes an empty string response to store the bot's response.
+It then checks if the user input matches any of the greetings by iterating over the keys of the greetings dictionary. If a greeting is found in the user input, the corresponding response is assigned to the response variable.
+Next, it checks if the user input matches any of the queries by iterating over the keys of the queries dictionary. If a query is found in the user input, the corresponding response is assigned to the response variable.
+If no specific response is found, it assigns a generic response indicating that the input could not be understood.
+Finally, it returns the response.
+
+The code includes an example usage loop that simulates a conversation with the chatbot:
+
+It starts by printing a greeting from the bot.
+Inside the while loop, it prompts the user for input using input("User: ").
+The user's input is then passed to the get_response function to obtain the bot's response.
+The bot's response is printed using print("Bot:", bot_response).
+The loop continues to prompt for user input and generate bot responses until it is interrupted or terminated.
+
+Overall, the code provides a basic structure for a chatbot that can understand and respond to user inputs based on predefined greetings and queries. It can be expanded and customized by adding more greetings, queries, and corresponding responses to make the chatbot more interactive and useful.  '''
